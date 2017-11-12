b96122983905e13ee44028bb55dcf02ad3de318b
Jordan Schaenzle <jschaenzle@gmail.com>
2017-11-12 14:36:15 -0500

    QUELL - fixed response

--- quell.rb
+++ quell.rb
@@ -1,6 +1,14 @@
 
 class RatePainSession
+
   def rate_pain
-    "On a scale of one to ten, how would you rate your pain over the last 24 hours?"
+    text_to_ssml "On a scale of one to ten, how would you rate your pain over the last 24 hours?"
   end
+
+  def text_to_ssml(text)
+    result = OPENING_TAG
+    result << text
+    result << CLOSING_TAG
+  end
+
 end
